7ee08e45ae1379b5b5098ac58eb9fc5d83ea34ad
MarcosEMAraujo <104570916+MarcosEMAraujo@users.noreply.github.com>
2022-08-25 19:21:36 -0300

    Add files via upload

---
+++ TIQ1.java
@@ -0,0 +1,8 @@
+class TIQ1{
+    public static void main(String[] args) {
+        int numero1 = 5,numero2 = 10;
+        int soma;
+        soma = numero1 + numero2;
+        System.out.println(soma);
+    }
+}
